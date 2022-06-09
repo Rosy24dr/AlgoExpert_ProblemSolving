4c9ea86464d172137e8e95e9eef9b7de225dccf7
Rosy24dr <Rosy24dr@outlook.com>
2022-06-09 00:48:45 -0400

    second solution to problem one

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
-# Problem No. 1
+# Problem No. 1 Solution 1
 
+# 0(n^2) time | 0(1) space
 array = [3, 5, -4, 8, 11, 1, -1, 6]
 targetResult = 10
 
@@ -11,3 +12,25 @@ def twoNumberSum(array, targetSum):
     return []
 
 print(twoNumberSum(array,targetResult))
+
+
+# Problem No. 1 Solution 2
+
+# 0(nLog(n)) time | O(1) space
+array = [3, 5, -4, 8, 11, 1, -1, 6]
+targetResult = 10
+
+
+def twoNumberSum(array, targetSum):
+    array.sort()
+    left = 0
+    right = len(array) - 1
+    while left < right:
+        currentSum = array[left] + array[right]
+        if currentSum == targetSum:
+            return [array[left], array[right]]
+        elif currentSum < targetSum:
+            left += 1
+        elif currentSum > targetSum:
+            right -= 1
+    return []
