e7130eb8782f18b17704dfee8ee996c57c9231ac
Rosy24dr <Rosy24dr@outlook.com>
2022-06-10 00:43:48 -0400

    solved problem 2

--- index.py
+++ index.py
@@ -34,3 +34,15 @@ def twoNumberSum(array, targetSum):
         elif currentSum > targetSum:
             right -= 1
     return []
+
+
+#Problem No. 2 
+
+def validateSubsequence(array, sequence):
+    seqIdx = 0
+    for value in array:
+        if seqIdx == len(sequence):
+            break
+        if sequence[seqIdx] == value:
+            seqIdx += 1
+    return seqIdx == len(sequence)
